e9b230c9b7c5c1e47e0295ee7af480c5dfcaf1a3
lakimancic <lazarskolski@gmail.com>
2023-12-04 07:02:50 +0100

    Day 4 Part 2

--- src/puzzles/solver04.rs
+++ src/puzzles/solver04.rs
@@ -7,11 +7,15 @@ pub fn solve() {
     const FILE_PATH: &str = "assets/input04.txt";
 
     let file = File::open(FILE_PATH).expect("Failed to open input file!");
+    let reader = BufReader::new(file);
+    let lines_count = reader.lines().count();
 
+    let file = File::open(FILE_PATH).expect("Failed to open input file!");
     let reader = BufReader::new(file);
     let mut sum_of_wins:u64 = 0;
+    let mut wins_dp = vec![1; lines_count];
 
-    for (_, line) in reader.lines().enumerate() {
+    for (j, line) in reader.lines().enumerate() {
         match line {
             Ok(content) => {
                 let splits: Vec<&str> = content.split(":").collect();
@@ -21,6 +25,7 @@ pub fn solve() {
 
                 let winnings:Vec<&str> = re.captures_iter(splits[0]).map(|x| x.get(0).unwrap().as_str()).collect();
                 let mut num_of_wins: u64 = 0;
+                let mut cnt_of_wins: usize = 0;
 
                 for capture in re.captures_iter(splits[1]) {
                     if winnings.contains(&capture.get(0).unwrap().as_str()) {
@@ -30,7 +35,12 @@ pub fn solve() {
                         else {
                             num_of_wins *= 2;
                         }
+                        cnt_of_wins += 1;
                     }
+                }
+
+                for i in 0..cnt_of_wins {
+                    wins_dp[j+i+1] += wins_dp[j];
                 }
 
                 sum_of_wins += num_of_wins;
@@ -42,4 +52,11 @@ pub fn solve() {
     }
 
     println!("Part 1 solution is: {}", sum_of_wins);
+
+    sum_of_wins = 0;
+    for num_of_wins in wins_dp {
+        sum_of_wins += num_of_wins;
+    }
+
+    println!("Part 2 solution is: {}", sum_of_wins);
 }
