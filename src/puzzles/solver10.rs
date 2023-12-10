6707ed03672e52adcbb278bccd6850b5a4ae5de9
lakimancic <lazarskolski@gmail.com>
2023-12-10 18:12:29 +0100

    Day 10 Part 2

--- src/puzzles/solver10.rs
+++ src/puzzles/solver10.rs
@@ -12,7 +12,7 @@ fn check_dirs(dir1: (i32, i32), dir2: &Vec<(i32, i32)>) -> bool {
 pub fn solve() {
     const FILE_PATH: &str = "assets/input10.txt";
     let txt = std::fs::read_to_string(FILE_PATH).unwrap();
-    let mat: Vec<Vec<char>> = txt.lines().map(|x| x.chars().collect::<Vec<char>>()).collect();
+    let mut mat: Vec<Vec<char>> = txt.lines().map(|x| x.chars().collect::<Vec<char>>()).collect();
     let n = mat.len() as i32;
     let m = mat[0].len() as i32;
 
@@ -23,7 +23,7 @@ pub fn solve() {
         ('J', vec![(-1, 0), (0, -1)]),
         ('7', vec![(1, 0), (0, -1)]),
         ('F', vec![(1, 0), (0, 1)]),
-        ('S', vec![(0, 1), (0, -1), (1, 0), (-1, 0)])
+        ('S', vec![(1, 0), (-1, 0), (0, 1), (0, -1)])
     ]);
     let mut s = (-1, -1, 0);
 
@@ -59,5 +59,50 @@ pub fn solve() {
     }
 
     println!("Part 1 solution is: {}", mx);
+
+    let mut start_dirs: Vec<(i32, i32)> = Vec::new();
+
+    for (di, dj) in dirs.get(&'S').unwrap() {
+        if s.0 + di < 0 || s.0 + di >= n { continue; }
+        if s.1 + dj < 0 || s.1 + dj >= m { continue; }
+        if mat[(s.0 + di) as usize][(s.1 + dj) as usize] == '.' { continue; }
+        if !check_dirs((*di, *dj), dirs.get(&mat[(s.0 + di) as usize][(s.1 + dj) as usize]).unwrap()) { continue; }
+
+        start_dirs.push((*di, *dj));
+    }
+
+    for (key, dir) in &dirs {
+        if *dir == start_dirs {
+            mat[s.0 as usize][s.1 as usize] = *key;
+        }
+    }
+
+    let mut br = 0;
+
+    for i in 0..n {
+        let mut is_in = false;
+        let mut cor_dir = (0, 0);
+
+        for j in 0..m {
+            let ch = mat[i as usize][j as usize];
+            if vis[i as usize][j as usize] {
+                if ch == 'L' || ch == 'F' {
+                    cor_dir = dirs.get(&ch).unwrap()[0];
+                }
+                else if ch == '7' || ch == 'J' {
+                    if check_dirs(cor_dir, dirs.get(&ch).unwrap()) {
+                        is_in = !is_in;
+                    }
+                }
+                else if ch == '|' {
+                    is_in = !is_in;
+                }
+            }
+            else {
+                br += if is_in { 1 } else { 0 };
+            }
+        }
+    }
     
+    println!("Part 2 solution is: {}", br);
 }
