9d28cf4ef68390c4d935354f3c75952483bbf4f1
lakimancic <lazarskolski@gmail.com>
2023-12-23 07:26:58 +0100

    Day 23 Part 2

--- src/puzzles/solver23.rs
+++ src/puzzles/solver23.rs
@@ -1,10 +1,17 @@
 static DIRS: [(i64, i64); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
 
-fn dfs(mat: &Vec<Vec<char>>, n: usize, m: usize, vis: &mut Vec<Vec<bool>>, pos: (i64, i64)) -> i64 {
+fn dfs(
+    mat: &Vec<Vec<char>>,
+    n: usize,
+    m: usize,
+    vis: &mut Vec<Vec<bool>>,
+    pos: (i64, i64),
+    part1: bool,
+) -> i64 {
     if pos.0 as usize == n - 1 && pos.1 as usize == m - 2 {
         return 0;
     }
-    if mat[pos.0 as usize][pos.1 as usize] != '.' {
+    if part1 && mat[pos.0 as usize][pos.1 as usize] != '.' {
         let new_pos = match mat[pos.0 as usize][pos.1 as usize] {
             '>' => (pos.0, pos.1 + 1),
             '<' => (pos.0, pos.1 - 1),
@@ -16,7 +23,7 @@ fn dfs(mat: &Vec<Vec<char>>, n: usize, m: usize, vis: &mut Vec<Vec<bool>>, pos: 
             return -1;
         }
         vis[new_pos.0 as usize][new_pos.1 as usize] = true;
-        let res = dfs(mat, n, m, vis, new_pos);
+        let res = dfs(mat, n, m, vis, new_pos, true);
         vis[new_pos.0 as usize][new_pos.1 as usize] = false;
         return if res != -1 { res + 1 } else { res };
     }
@@ -36,7 +43,7 @@ fn dfs(mat: &Vec<Vec<char>>, n: usize, m: usize, vis: &mut Vec<Vec<bool>>, pos: 
             continue;
         }
         vis[np.0 as usize][np.1 as usize] = true;
-        let new_res = dfs(mat, n, m, vis, np);
+        let new_res = dfs(mat, n, m, vis, np, part1);
         if new_res != -1 {
             res = std::cmp::max(res, new_res + 1);
         }
@@ -59,5 +66,12 @@ pub fn solve() {
 
     let mut vis = vec![vec![false; m]; n];
     vis[0][1] = true;
-    println!("Part 1 solution is: {}", dfs(&mat, n, m, &mut vis, (0, 1)));
+    println!(
+        "Part 1 solution is: {}",
+        dfs(&mat, n, m, &mut vis, (0, 1), true)
+    );
+    println!(
+        "Part 2 solution is: {}",
+        dfs(&mat, n, m, &mut vis, (0, 1), false)
+    );
 }
